424d1358d2fbd8dfbf329cf32922a40a669d2115
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-08 21:36:25 +0400

    Func vahe.py

--- vahe.py
+++ vahe.py
@@ -1,1 +1,9 @@
-print("Hello world")
+print("Enter a number")
+x = int(input())
+if x > 0:
+    print("positive number")
+else:
+    if x == 0:
+        print("number 0")
+    elif x < 0:
+        print("negative number")
