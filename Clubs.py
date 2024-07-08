cff6b61c2d280202d49d8ef5d72337c8404e7328
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-08 23:23:54 +0400

    Create Clubs.py

---
+++ Clubs.py
@@ -0,0 +1,12 @@
+print("Enter number")
+a = int(input())
+if a < 0:
+    print("wrong")
+elif a < 7:
+    print("1st")
+elif a < 13:
+    print("2nd")
+elif a < 17:
+    print("3rd")
+else:
+    print("4rd")
