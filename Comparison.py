b2ea4e7e1789b86db81a0d5a379a5c0944cb668a
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-08 23:22:10 +0400

    Create Comparison.py

---
+++ Comparison.py
@@ -0,0 +1,21 @@
+m = 5
+n = 4
+print(m == n)
+print(m > n)
+print(m != n)
+age = int(input())
+var = 6 <= age <= 18
+
+x = 9
+x = 12
+y = 25
+y = 7
+print(x > 10 and y > 10)
+print(x > 10 or y > 10)
+
+a = 4
+b = 11
+print(a > 5 and b > 5)
+print(a > 3 and b > 3)
+print(a > 10 or b > 10)
+print(a > 20 or b > 20)
