a5fbab3d030c1103cc6582608bfdc040fe97137c
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-10 13:23:28 +0400

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,18 @@
+def my_name():
+    print("Vahe")
+
+# my_name()
+
+def greetings(name):
+    print("Hi", name)
+
+greetings("Areg")
+
+
+def product(a,b):
+    S = a * b
+    print(S)
+
+product(5, 4)
+product(20, 13)
+product(3, 6)
